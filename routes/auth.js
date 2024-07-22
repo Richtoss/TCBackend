708f4b1addf5203f005715798a0f28731709a69c
Richtoss <CoryJRichter@gmail.com>
2024-07-22 15:31:47 -0500

    first commit

--- routes/auth.js
+++ routes/auth.js
@@ -112,4 +112,14 @@ router.post('/login', [
   }
 });
 
+router.get('/me', auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
 module.exports = router;
